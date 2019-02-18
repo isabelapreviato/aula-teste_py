70b90689f54b70328bd86ae51e9d6483f7d1f53f
isabelapreviato <isabelapreviato@hotmail.com>
2019-02-18 12:04:43 -0300

    primeiro

---
+++ teste.py
@@ -0,0 +1,69 @@
+# if 2 is 3:
+#     print('dois é igual a três')
+# else:
+#     print('dois não é igual a três')
+
+# print('Oi!')
+
+# # abc
+# # jdjkdsjkdkd
+# # dskdfnfdsjdsjk
+# # sakjkjdjkdsf  
+
+# lista = ['a', 'b', 'c', 1, 2, 3]
+# for item in lista:
+#     print(item)
+
+# string = 'isabela'
+# for letra in string:
+#     print(letra)
+
+# for i in range(len(string)):
+#     print()
+
+
+
+# Dois números
+
+n1 = float(input("Digite o número 1: "))
+n2 = float(input("Digite o número 2: "))
+
+soma = n1 + n2
+print(f"A soma dos dois números: {soma}")
+
+sub = n1 - n2
+print(f"A subtração dos dois números: {sub}")
+
+mult = n1 * n2
+print(f"A multiplicação dos dois números: {mult}")
+
+div = n1 / n2
+print(f"A divisão dos dois números: {div}")
+
+# Quatro números
+
+nn1 = float(input("Digite o número 1 de 4: "))
+nn2 = float(input("Digite o número 2 de 4: "))
+nn3 = float(input("Digite o número 3 de 4: "))
+nn4 = float(input("Digite o número 4 de 4: "))
+
+soma2 = nn1 + nn2 + nn3 + nn4
+print(f"A soma dos quatro números é: {soma2}")
+
+media = soma2 / 4
+print(f"A média dos quatro números é: {media}")
+
+# Temperatura
+
+Temp = float(input("Digite a temperatura em Celcius: "))
+Fah = (Temp * 9/5) + 32
+print(f"A temperatura em Fahrenheit é : {Fah}")
+
+# Dois inputs Palavra/Frase e uma Letra 
+
+frase = (input("Digite a frase a ser analisada: "))
+letra = (input("Digite a letra que será encontrada na frase: "))
+print(f"Foram encontradas {(frase.count(letra))} letras {letra} da sua frase: {frase}.")
+# print(frase.count(letra))
+
+# 
